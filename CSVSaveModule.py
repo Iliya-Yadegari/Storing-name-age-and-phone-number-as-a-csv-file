925619f849022195b96d2dc50cc0a455a55e9dc2
Iliya-Yadegari <naruto3@email.com>
2021-04-13 19:20:24 +0100

    Update CSVSaveModule.py

--- CSVSaveModule.py
+++ CSVSaveModule.py
@@ -1,6 +1,6 @@
 import csv
 
-def res_fun():
+def res_fun(row1,row2,row3):
     
     fields = ['name','age','phone number']
 
@@ -9,3 +9,7 @@ def res_fun():
         csvwriter = csv.writer(csvfile)
         
         csvwriter.writerow(fields)
+    with open('test.csv','a') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        rows = [row1,row2,row3]
+        csvwriter.writerow(rows)
